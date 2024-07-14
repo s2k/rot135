15c1d9c73731a85ac9e6477174b7925928f2508a
Stephan Kämper <the.tester@seasidetesting.com>
2024-07-14 19:40:15 +0200

    Set up Cucumber to use Minitest assertions
    Note: Make sure RSpec assertions are NOT installed gems when using this.
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
-# Put environment information here as needed.
+# Put environment & setup information here as needed.
+
+EXPECTATIONS_DIR = File.join( File.dirname(__FILE__), '..', 'expected_test_data')
+
+require 'minitest'
+
+ROT135_CMD = File.join(File.dirname(__FILE__), '../../exe/rot135')
+
+# Provide a Minitest world and initialise it so that Cucumber can use it
+class MinitestWorld
+  extend Minitest::Assertions
+  attr_accessor :assertions
+
+  def initialize
+    self.assertions = 0
+  end
+end
+
+World do
+  MinitestWorld.new
+end
