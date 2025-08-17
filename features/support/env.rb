e389c6a3df88312b9a67cdc152434cdc638af53f
Stephan Kämper <the.tester@seasidetesting.com>
2025-08-17 14:32:29 +0200

    Follows style guide
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,6 +11,7 @@ ROT135_CMD = File.join(File.dirname(__FILE__), '../../exe/rot135')
 # Provide a Minitest world and initialise it so that Cucumber can use it
 class MinitestWorld
   extend Minitest::Assertions
+
   attr_accessor :assertions
 
   def initialize
