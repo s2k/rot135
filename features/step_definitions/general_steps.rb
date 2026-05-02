cf63f6d2e393f905275b9351683d3fd35ee3947e
Stephan Kämper <the.tester@seasidetesting.com>
2026-05-02 18:54:59 +0200

    Updates/adds step definitions

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
-Given('rot135 is run with {string} only') do |options|
-  @cmd_output = `#{ROT135_CMD} #{options}`
+Given('rot135 is run without input and option {string}') do |option|
+  @cmd_output = `#{ROT135_CMD} #{option}`
+end
+
+Given('rot135 is run with input {string} and option {string}') do |input, option|
+  @cmd = "echo #{input} | #{ROT135_CMD} #{option}"
+
+  @runner = CommandRunner.new(@cmd)
+  @stdout, @stderr, @status = @runner.run
 end
 
 Then('the output should be as given in file {string}') do |_string|
   expected_text = File.read(File.join(EXPECTATIONS_DIR, 'help_output.txt'), chomp: true).strip
   assert_equal expected_text, @cmd_output.strip
+end
+
+Then('STDOUT should match gem version') do
+  assert_equal "rot135 version #{Rot135::VERSION}", @stdout.strip, "Unexpected restult for command #{@cmd}"
 end
--- features/step_definitions/handle_input_from_stdin.rb
+++ features/step_definitions/handle_input_from_stdin.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
 
-Given('{string} is fed in via STDIN') do |input|
-  @output = `echo #{input} | ./exe/rot135`
+require 'amazing_print'
+
+def context_details
+  "STDOUT: #{@stdout.inspect}, STDERR: #{@stderr.inspect} ExitCode: #{@status.inspect})"
 end
 
-Then('the output should be {string}') do |expected_text|
-  assert_equal expected_text, @output.strip
+Given('{string} is fed in via STDIN') do |input|
+  cmd = "echo #{input} | ./exe/rot135"
+  begin
+    @stdout, @stderr, @status = CommandRunner.new(cmd).run
+  rescue StandardError => e
+    puts '!?!? WHAAT?!'
+    ap e
+  end
+end
+
+Then('STDOUT should be {string}') do |expected_text|
+  refute @stdout.nil?, "Expected some content in STDOUT, but got #{context_details}"
+  assert_equal 0, @status.exitstatus, "Expected exit code 0 but got #{context_details}"
+  assert_equal expected_text, @stdout.strip, "Got #{@stdout.inspect}, #{@stderr.inspect} and #{@status.inspect} instead of #{expected_text}"
 end
